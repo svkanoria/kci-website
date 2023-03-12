7f7f25fc1bd63659ced884f61613280ddf936c1d
Saumya Vardhan Kanoria <saumya.kanoria@gmail.com>
2023-03-12 08:50:20 +0530

    Switches to Cloudinary for images

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,14 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   plugins: [require("daisyui")],
   daisyui: {
-    themes: ["light"],
+    themes: [
+      {
+        light: {
+          ...require("daisyui/src/colors/themes")["[data-theme=light]"],
+          primary: "darkblue",
+          "primary-focus": "mediumblue",
+        },
+      },
+    ],
   },
 };
