9a484b06dc42be6f40cfde7b9efc7af272b4a0ed
Saumya Vardhan Kanoria <saumya.kanoria@gmail.com>
2023-04-27 01:23:59 +0530

    Adds corporate group section to home page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,11 +3,14 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
   daisyui: {
+    // https://daisyui.com/docs/themes
     themes: [
       {
         light: {
           ...require("daisyui/src/colors/themes")["[data-theme=light]"],
           primary: "darkblue",
+          "--rounded-btn": "0rem",
+          // Added by us
           "primary-focus": "mediumblue",
         },
       },
