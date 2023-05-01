a5964dbaa381264b324361143daec1a999ab4e33
Saumya Vardhan Kanoria <saumya.kanoria@gmail.com>
2023-05-01 13:18:35 +0530

    Changes site font to Raleway

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,15 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Raleway", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
   daisyui: {
     // https://daisyui.com/docs/themes
