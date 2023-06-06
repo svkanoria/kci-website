dd7e7e2f7910585b4f1252b34639231b94dbd312
Saumya Vardhan Kanoria <saumya.kanoria@gmail.com>
2023-06-06 15:12:40 +0530

    Adds left-shift to NavLayout for bigger screens

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,6 +8,14 @@ module.exports = {
       fontFamily: {
         sans: ["Raleway", ...defaultTheme.fontFamily.sans],
       },
+      screens: {
+        // Since having the left nav makes the body quite thin, we end up with
+        // a thin column-like body shifted far to the right side of the page.
+        // In such cases, it is better to shift everything (nav and body both)
+        // a little to the left, so as to reduce the right-shift of the body.
+        // 1168 = 1024 + 288/2 (nav width is w-72, which is equivalent to 288px)
+        navshift: "1168px",
+      },
     },
   },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
