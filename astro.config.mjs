3d7834bffb618850b41ce1042c2e59073ec3b8df
Saumya Vardhan Kanoria <saumya.kanoria@gmail.com>
2023-07-03 22:13:13 +0530

    Adds sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,12 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import storyblok from "@storyblok/astro";
 import { loadEnv } from "vite";
-
+import sitemap from "@astrojs/sitemap";
 const env = loadEnv("", process.cwd(), "STORYBLOK");
 
+// https://astro.build/config
 export default defineConfig({
+  site: "https://d23mqz9f511gr7.cloudfront.net",
   integrations: [
     tailwind(),
     storyblok({
@@ -20,5 +22,6 @@ export default defineConfig({
         // region: "us", // optional,  or 'eu' (default)
       },
     }),
+    sitemap(),
   ],
 });
